6cac27f1946996fe2c856e2296f43541bda0f1f4
eatingfoil@gmail.com <eatingfoil@gmail.com>
2018-05-14 23:33:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,8 @@ function deleteFromObjectByKey(object, key) {
   delete newRecipe[key];
   return newRecipe
 }
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete.object[key];
+  return object
+}
